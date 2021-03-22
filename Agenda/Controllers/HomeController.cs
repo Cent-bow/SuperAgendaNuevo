11e303ace3d4b0e689beed6349042ab030bf3c2e
Centhia Bowman <Centhiabowman@outlook.com>
2021-03-22 09:20:25 -0400

    Ok

--- Agenda/Controllers/HomeController.cs
+++ Agenda/Controllers/HomeController.cs
@@ -12,16 +12,59 @@ namespace Agenda.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly dbContactosContext _db;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, dbContactosContext db)
         {
             _logger = logger;
+            _db = db;
         }
 
         public IActionResult Index()
         {
+           
+            var contactos = _db.Contactos;
+            return View(contactos);
+        }
+
+        public IActionResult Agregar()
+        {
             return View();
         }
+
+        public IActionResult Agregar (Contacto Input)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Contactos.Add (Input);
+                _db.SaveChanges();
+            }
+            return View(Input);
+        }
+
+        public IActionResult Modificar (int id)
+        {
+            var output = _db.Contactos.Find(id);
+            return View(output);
+        }
+
+        [HttpPost]
+
+        public IActionResult Modificar (Contacto input)
+        {
+
+            if (ModelState.IsValid)
+            {
+                _db.Entry(input).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+                _db.SaveChanges();
+            }
+
+            return View(input);
+        }
+
+
+
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
---
+++ Agenda/Migrations/20210318125312_crear_tabla_contactos.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Agenda.Migrations
+{
+    public partial class crear_tabla_contactos : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Contactos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombres = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Apellidos = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    TelefonoMovil = table.Column<string>(type: "nvarchar(14)", maxLength: 14, nullable: false),
+                    TelefonoCasa = table.Column<string>(type: "nvarchar(14)", maxLength: 14, nullable: true),
+                    TelefonoTrabajo = table.Column<string>(type: "nvarchar(14)", maxLength: 14, nullable: true),
+                    Empresa = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Direccion = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Cedula = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Correo = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    FechaNacimiento = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Instagram = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Facebook = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PaginaWeb = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PuestoTrabajo = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Foto = table.Column<byte[]>(type: "varbinary(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Contactos", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Contactos");
+        }
+    }
+}
